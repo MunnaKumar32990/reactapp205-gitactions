42ec09dfa36a250e032dd741ecf443b74ccdc98d
MunnaKumar32990 <2300032990@kluniversity.in>
2025-07-28 10:13:34 +0530

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import 'style.css' 
+import React from 'react';
+import './style.css';
+
 function App() {
   return (
     <div className='container'>
       <h1>Welcome to My React Project s-205</h1>
       <p>This is a simple Page deployed on GitHub Pages using Vite</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
