140198fc38a3215945f5a5240c8efc3daf0fdd37
MunnaKumar32990 <2300032990@kluniversity.in>
2025-07-28 10:02:36 +0530

    react project v1.0

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import 'src/style.css' 
+function App() {
+  return (
+    <div className='container'>
+      <h1>Welcome to My React Project s-205</h1>
+      <p>This is a simple Page deployed on GitHub Pages using Vite</p>
+    </div>
+  )
+}
+
+export default App
